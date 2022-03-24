0e3f5bc27d350b1692f40792096bc8e699974edb
Klint Keown <klintkeown@yahoo.com>
2022-03-24 02:01:49 -0500

    initial commit

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,27 @@
+import logo from '../assets/logo.png';
+import profile2 from '../assets/profile2.png';
+import circle from '../assets/circle.svg';
+import bgIMG from '../assets/bgIMG.png';
+import flutter from '../assets/flutter.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import react from '../assets/react.png';
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+export default {
+    logo,
+    profile2,
+    circle,
+    bgIMG,
+    flutter,
+    redux,
+    sass,
+    react,
+    about01,
+    about02,
+    about03,
+    about04,
+};
