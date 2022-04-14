cbe98f99539371981a4033c433626b8369330fc5
Klint Keown <KKeown@AWP.local>
2022-04-14 15:35:13 -0500

    working on uncaught exception error

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.facebook.com/klint.keown">
+        <FaFacebookF />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
+      <a href="https://www.instagram.com/cobaltbass">
+        <BsInstagram />
+      </a>
     </div>
   </div>
 );
